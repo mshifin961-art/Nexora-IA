cf06681dc7bc65a137fe4c6cd54bcc75daad0b4f
Muhammed Shifin <Mshifin961@gmail.com>
2026-07-22 20:17:42 +0530

    Initial Nexora AI project

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#0F172A'
+    }}>
+      <Text style={{
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold'
+      }}>
+        Nexora AI 🚀
+      </Text>
+    </View>
+  );
+}
+2
+
+
