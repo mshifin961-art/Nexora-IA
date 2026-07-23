82b2eeca30e951dcd73f9f7478f6694783c7929e
mshifin961-art <mshifin961@gmail.com>
2026-07-23 17:35:47 +0530

    App.js

--- App.js
+++ App.js
@@ -1,13 +1,63 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.logo}>🤖</Text>
-      <Text style={styles.title}>Nexora AI</Text>
-      <Text style={styles.subtitle}>The Future Starts Here</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Text style={styles.title}>🏪 BillNova AI</Text>
+        <Text style={styles.subtitle}>Smarter Billing. Better Business.</Text>
+
+        <View style={styles.salesCard}>
+          <Text style={styles.cardTitle}>Today's Sales</Text>
+          <Text style={styles.amount}>₹12,540</Text>
+          <Text style={styles.smallText}>24 Bills Today</Text>
+        </View>
+
+        <TouchableOpacity style={styles.billButton}>
+          <Text style={styles.billButtonText}>+ Create New Bill</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.section}>Quick Actions</Text>
+
+        <View style={styles.grid}>
+          <View style={styles.box}>
+            <Text style={styles.icon}>📦</Text>
+            <Text style={styles.boxText}>Products</Text>
+          </View>
+
+          <View style={styles.box}>
+            <Text style={styles.icon}>👥</Text>
+            <Text style={styles.boxText}>Customers</Text>
+          </View>
+
+          <View style={styles.box}>
+            <Text style={styles.icon}>📊</Text>
+            <Text style={styles.boxText}>Reports</Text>
+          </View>
+
+          <View style={styles.box}>
+            <Text style={styles.icon}>🤖</Text>
+            <Text style={styles.boxText}>AI Insights</Text>
+          </View>
+        </View>
+
+        <View style={styles.aiCard}>
+          <Text style={styles.aiTitle}>🤖 AI Insight</Text>
+          <Text style={styles.aiText}>
+            Your top-selling product today is Rice.
+          </Text>
+        </View>
+
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -15,21 +65,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#0F172A',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  logo: {
-    fontSize: 70,
-    marginBottom: 20,
+    padding: 20,
   },
   title: {
-    color: '#FFFFFF',
-    fontSize: 34,
+    color: '#fff',
+    fontSize: 30,
     fontWeight: 'bold',
+    marginTop: 10,
   },
   subtitle: {
     color: '#94A3B8',
-    fontSize: 16,
-    marginTop: 10,
+    marginBottom: 25,
   },
-});
+  salesCard: {
+    backgroundColor: '#1E293B',
+    padding: 20,
+    borderRadius: 18,
+    marginBottom: 
