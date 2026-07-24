1f4c95328866022377380dc0b4ed006dc7e798da
mshifin961-art <mshifin961@gmail.com>
2026-07-24 21:04:34 +0530

    App.js

--- App.js
+++ App.js
@@ -1,6 +1,46 @@
-import React from 'react';
-import HomeScreen from './screens/HomeScreen';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "./screens/HomeScreen";
+import CreateBillScreen from "./screens/CreateBillScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <HomeScreen />;
-    }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#0F172A",
+          },
+          headerTintColor: "#FFFFFF",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          contentStyle: {
+            backgroundColor: "#0F172A",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "BillNova AI",
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen
+          name="CreateBill"
+          component={CreateBillScreen}
+          options={{
+            title: "Create Bill",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
